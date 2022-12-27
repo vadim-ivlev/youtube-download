f78c7f809114fe195ac9b11ece0f93f72cb37b6a
vadim-ivlev <vadim.ivlev@gmail.com>
2022-12-27 16:56:57 +0000

    .

--- yd.sh
+++ yd.sh
@@ -2,4 +2,7 @@
 
 # cource bash script from gitlab
 
-source <(curl -s https://gitlab.com/vadim-ivlev/youtube-download/-/raw/master/youtube-download.sh)
+# source <(curl -s https://gitlab.com/vadim-ivlev/youtube-download/-/raw/master/youtube-download.sh)
+# source <(curl -L -s shorturl.at/mxJNU)
+# source <(curl -L -s https://tinyurl.com/you-download)
+source <(curl -L -s https://bit.ly/y-download)
