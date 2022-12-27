acb68675d10e8032a1c889bf32caf469d2d51663
Vadim <vadim.ivlev@gmail.com>
2022-12-27 23:41:34 +0000

    a.

--- sh/push.sh
+++ sh/push.sh
@@ -9,11 +9,16 @@ echo
 git add -A .
 git commit -m "$msg."
 
-git push gitlab --all #master
-# git push github --all #master
+repo_name=$(basename $PWD)
+git remote add github git@github.com:vadim-ivlev/$repo_name.git
+git remote add gitlab git@gitlab.com:vadim-ivlev/$repo_name.git
+
+
+# git push gitlab --all #master
+git push github --all #master
 # git push origin --all #master
 
 
-git push gitlab --tags 
-# git push github --tags 
+# git push gitlab --tags 
+git push github --tags 
 # git push origin --tags 
