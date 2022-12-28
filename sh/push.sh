ff70f0f2bfd8ea3225a933b68afef12a6585fc7a
Vadim <vadim.ivlev@gmail.com>
2022-12-28 00:28:13 +0000

    aa.

--- sh/push.sh
+++ sh/push.sh
@@ -9,16 +9,20 @@ echo
 git add -A .
 git commit -m "$msg."
 
-repo_name=$(basename $PWD)
-git remote add github git@github.com:vadim-ivlev/$repo_name.git
-git remote add gitlab git@gitlab.com:vadim-ivlev/$repo_name.git
+# repo_name=$(basename $PWD)
+# git remote add github git@github.com:vadim-ivlev/$repo_name.git
+# git remote add gitlab git@gitlab.com:vadim-ivlev/$repo_name.git
+# git remote add origin https://[USERNAME]:[TOKEN]@git.mycompany.com/[ORGANIZATION]/[REPO].git
 
 
 # git push gitlab --all #master
-git push github --all #master
+# git push github --all #master
 # git push origin --all #master
 
 
 # git push gitlab --tags 
-git push github --tags 
+# git push github --tags 
 # git push origin --tags 
+
+git push https://vadim-ivlev:$GHUB_ACCESS_TOKEN@github.com/vadim-ivlev/youtube-download.git --all
+git push https://vadim-ivlev:$GHUB_ACCESS_TOKEN@github.com/vadim-ivlev/youtube-download.git --tags
